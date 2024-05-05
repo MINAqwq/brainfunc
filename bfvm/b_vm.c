d6baa53412868933ee1716019f537eac5d8a8db6
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2024-05-05 22:05:26 +0200

    add call and ret fix, read and sanity checks

--- bfvm/b_vm.c
+++ bfvm/b_vm.c
@@ -46,6 +46,11 @@ bfvm_code_load_from_file(BfVm **vm, const char *file_path)
 	/* read header */
 	fread(&header, sizeof(BfExecHeader), 1, fp);
 
+	if (header.magic[0] != 'B' || header.magic[1] != 'F' ||
+	    header.magic[2] != 'U' || header.magic[3] != 'N') {
+		return 1;
+	}
+
 	/* resize vm */
 	*vm = bf_realloc(*vm, sizeof(**vm) - sizeof(char) + file_size,
 			 "VM Resize", "realloc for code from file");
@@ -71,9 +76,8 @@ bfvm_loop_start(BfVm *vm)
 {
 	if (!(vm->memory[vm->index_memory])) {
 		while (vm->code[vm->index_code++] != BF_BYTE_LOOP_END) {
-			fputc('.', stderr);
-			fflush(stderr);
 		}
+
 		fputc('\n', stderr);
 
 		return;
@@ -108,14 +112,20 @@ bfvm_loop_exit(BfVm *vm)
 void
 bfvm_call(BfVm *vm)
 {
-	vm->callstack[++vm->index_callstack] =
-	    vm->code[vm->index_code + BF_SIZE_ADDRESS + 1];
-	vm->index_code = ((uint)vm->code[vm->index_code + 1]);
+#ifdef BF_DEBUG
+	fprintf(stderr, "CALL %08X\n", ((uint)vm->code[vm->index_code + 1]));
+#endif
+
+	vm->callstack[++vm->index_callstack] = vm->index_code + BF_SIZE_ADDRESS;
+	vm->index_code = (*((uint *)(vm->code + vm->index_code + 1))) - 1;
 }
 
 void
 bfvm_return(BfVm *vm)
 {
+#ifdef BF_DEBUG
+	fprintf(stderr, "RET  %08X\n", vm->callstack[vm->index_callstack]);
+#endif
 	vm->index_code = vm->callstack[vm->index_callstack--];
 }
 
@@ -125,8 +135,6 @@ bfvm_exec(BfVm *vm)
 	BfByte opcode;
 	while (1) {
 		opcode = vm->code[vm->index_code];
-		/* fprintf(stderr, "[%04Xe]opcode: %X\n", vm->index_code,
-		 * opcode); */
 		switch (opcode) {
 		case BF_BYTE_NONE:
 			return 1;
@@ -138,25 +146,46 @@ bfvm_exec(BfVm *vm)
 			break;
 		case BF_BYTE_LEFT:
 			vm->index_memory--;
+			if (vm->index_memory > sizeof(vm->memory)) {
+				return 1;
+			}
 			break;
 		case BF_BYTE_RIGHT:
 			vm->index_memory++;
+			if (vm->index_memory > sizeof(vm->memory)) {
+				return 1;
+			}
 			break;
 		case BF_BYTE_LOOP_START:
+			if ((vm->index_callstack + 1) >
+			    (sizeof(vm->callstack) / sizeof(uint))) {
+				return 1;
+			}
+
 			bfvm_loop_start(vm);
 			break;
 		case BF_BYTE_LOOP_END:
+			if ((vm->index_callstack + 1) >
+			    (sizeof(vm->callstack) / sizeof(uint))) {
+				return 1;
+			}
+
 			bfvm_loop_exit(vm);
 			break;
 		case BF_BYTE_PRINT:
 			fputc(vm->memory[vm->index_memory], stdout);
 			break;
 		case BF_BYTE_READ:
+			vm->memory[vm->index_memory] = fgetc(stdin);
 			break;
 		case BF_BYTE_CALL:
 			bfvm_call(vm);
 			break;
 		case BF_BYTE_RETURN:
+			if (!vm->index_callstack) {
+				return 0;
+			}
+
 			bfvm_return(vm);
 			break;
 		default:
